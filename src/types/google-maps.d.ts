336127f14a340afdc52528e004914b278a8d32ff
rmondo <ryosuke.mondo@gmail.com>
2025-07-19 21:14:25 +0900

    added types

--- src/types/google-maps.d.ts
+++ src/types/google-maps.d.ts
@@ -9,4 +9,47 @@ declare global {
   }
 }
 
-export {};
+/**
+ * Configuration interface for Google Maps initialization
+ */
+export interface MapConfig {
+  center: google.maps.LatLngLiteral;
+  zoom: number;
+  mapTypeId?: google.maps.MapTypeId;
+}
+
+/**
+ * Props interface for GoogleMap component
+ */
+export interface GoogleMapProps {
+  config: MapConfig;
+  className?: string;
+  onMapLoad?: (map: google.maps.Map) => void;
+}
+
+/**
+ * Props interface for MapContainer component
+ */
+export interface MapContainerProps {
+  children: React.ReactNode;
+  height?: string | number;
+  className?: string;
+}
+
+/**
+ * Interface for Google Maps API loading utility
+ */
+export interface GoogleMapsLoaderOptions {
+  apiKey: string;
+  libraries?: string[];
+  version?: string;
+}
+
+/**
+ * Interface for map loading state
+ */
+export interface MapLoadingState {
+  isLoading: boolean;
+  isLoaded: boolean;
+  error: string | null;
+}
